19b491d6e414ee89636b4184bd2783138580b24f
Naxoht <nhidalgotoca@gmail.com>
2021-11-05 18:29:52 +0100

    Application with tabs

---
+++ app/src/main/java/com/example/appkahootadi/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,53 @@
+package com.example.appkahootadi.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.appkahootadi.Connection;
+import com.example.appkahootadi.R;
+import com.example.appkahootadi.Ranking;
+import com.example.appkahootadi.User;
+
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2,R.string.tab_text_3};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new Connection();
+            case 1:
+                return new User();
+            case 2:
+                return new Ranking();
+            default:
+                return new PlaceholderFragment();
+        }
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 3;
+    }
+}

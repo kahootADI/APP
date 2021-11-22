be07577807aa9b61ce46d59419a2a19e801751ea
Naxoht <nhidalgotoca@gmail.com>
2021-11-22 19:11:05 +0100

    App sends nickname, also changes semafor image to green

--- app/src/main/java/com/example/appkahootadi/Connection.java
+++ app/src/main/java/com/example/appkahootadi/Connection.java
@@ -1,6 +1,8 @@
 package com.example.appkahootadi;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -21,8 +23,11 @@ import java.io.IOException;
 import lipermi.handler.CallHandler;
 import lipermi.net.Client;
 
-
-public class Connection extends Fragment implements View.OnClickListener{
+interface TestService {
+    public String getNickname(String nickname);
+    public boolean kahootStarted();
+}
+public class Connection extends Fragment implements TestService {
     private Button btn;
     private EditText serverIP;
     Handler handler;
@@ -61,17 +66,30 @@ public class Connection extends Fragment implements View.OnClickListener{
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_connection, container, false);
         btn = (Button) v.findViewById(R.id.btn);
-        btn.setOnClickListener(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                new Conn().execute();
+            }
+        });
         return v;
 
     }
-    public void onClick(View v){
 
-        new Conn().execute();
+
+    @Override
+    public String getNickname(String nickname) {
+        return nickname;
+    }
+
+    @Override
+    public boolean kahootStarted() {
+        return false;
     }
 
     class Conn extends AsyncTask<ImageView, Void, ImageView> {
         ImageView iv;
+        boolean connected = false;
         @Override
         protected ImageView doInBackground(ImageView... params) {
             Looper.prepare();
@@ -80,16 +98,34 @@ public class Connection extends Fragment implements View.OnClickListener{
                 serverIP = (EditText) getView().findViewById(R.id.textIp);
                 String ip = serverIP.getText().toString();
                 Client client = new Client(ip, 7777, callHandler);
-                //TestService testService = (TestService) client.getGlobal(TestService.class);
-                //testService.getResponse("Hola");
-                //Toast.makeText(getContext(), testService.getResponse("Pepe"), Toast.LENGTH_SHORT).show();
-                Toast.makeText(getContext(), "Servidor Disponible", Toast.LENGTH_SHORT).show();
-                handler.post(new Runnable() {
-                    public void run() {
-                        iv = getView().findViewById(R.id.image);
-                        iv.setImageResource(R.drawable.orange);                    }
-                });
-                //client.close();
+                TestService testService = (TestService) client.getGlobal(TestService.class);
+                SharedPreferences preferences = getContext().getSharedPreferences("credentials", Context.MODE_PRIVATE);
+                if(!preferences.getString("user","").equals("")) {
+                    connected = true;
+                    testService.getNickname(preferences.getString("user",""));
+                    Toast.makeText(getContext(), "Servidor Disponible", Toast.LENGTH_SHORT).show();
+                    if(testService.kahootStarted()) {
+                        handler.post(new Runnable() {
+                            public void run() {
+                                iv = getView().findViewById(R.id.image);
+                                iv.setImageResource(R.drawable.green);
+                            }
+                        });
+                    }else{
+                        handler.post(new Runnable() {
+                            public void run() {
+                                iv = getView().findViewById(R.id.image);
+                                iv.setImageResource(R.drawable.orange);
+                            }
+                        });
+                    }
+                }else{
+                    Toast.makeText(getContext(), "Error de Conexió,Nickname incorrecte", Toast.LENGTH_SHORT).show();
+                }
+                if(!connected){
+                    Toast.makeText(getContext(), "Servidor no Disponible", Toast.LENGTH_SHORT).show();
+
+                }
             } catch (IOException e) {
                 Toast.makeText(getContext(), "Servidor no Disponible", Toast.LENGTH_SHORT).show();
             }
@@ -97,9 +133,5 @@ public class Connection extends Fragment implements View.OnClickListener{
             return iv;
         }
 
-    }
-
-    public interface TestService {
-        public String getResponse(String data);
     }
 }
